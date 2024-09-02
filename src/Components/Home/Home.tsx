7b1b6f5588b6f888f9977f3451ab0a5d9657564f
Iconians <claytoncripe@gmail.com>
2024-09-02 10:58:20 -0700

    deleted a few imports and code from home.tsx

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,51 +1,15 @@
-import { Fade } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import "./Home.css";
 import PortfolioSection from "../PortfolioSection/PortfolioSection";
 import Modal from "../Modal/Modal";
-import { portfolioItems } from "../../utils/PorfolioItems";
 import { JokeAdviceComponent } from "../Joke&AdviceComponent/JokeAdviceComponent";
 import { ReviewComponent } from "../ReviewComponet/ReviewComponent";
-import { ScheduleModal } from "../ScheduleModal/ScheduleModal";
-import { AboutModal } from "../AboutModal/AboutModal";
 import { PhotoCarousel } from "../PhotoCarousel/PhotoCarousel";
 import Nav from "../Nav/Nav";
 import { ContinuedLearning } from "../ContinuedLearning/ContinuedLearning";
 import { ArticlesComponent } from "../ArticlesComponent/ArticlesComponent";
 
 export const Home = () => {
-  const buttonStyle = {
-    width: "30px",
-    background: "none",
-    border: "0px",
-    cursor: "default",
-  };
-
-  const properties = {
-    prevArrow: (
-      <button style={{ ...buttonStyle }}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-          fill="#fff"
-        >
-          <path d="M242 180.6v-138L0 256l242 213.4V331.2h270V180.6z" />
-        </svg>
-      </button>
-    ),
-    nextArrow: (
-      <button style={{ ...buttonStyle }}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-          fill="#fff"
-        >
-          <path d="M512 256L270 42.6v138.2H0v150.6h270v138z" />
-        </svg>
-      </button>
-    ),
-  };
-
   return (
     <div className="App">
       <header className="">
