8ff80192092af55544cb55230c60ef2a9d52f269
Iconians <claytoncripe@gmail.com>
2025-09-01 19:21:59 -0700

    marked blogpostClient as a client side

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -15,6 +15,7 @@ export default async function BlogPost({ params }: BlogPageProps) {
   return (
     <div className={styles.page}>
       <Nav />
+      {/* mark BlogPostClient as client component */}
       <BlogPostClient frontMatter={frontMatter} mdxSource={mdxSource} />
     </div>
   );
