a52fc1fd738954d4de534cb850e051c8c214ffff
Iconians <claytoncripe@gmail.com>
2025-10-19 19:33:42 -0700

    removed the modal from the animation section and updated a package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,9 +31,9 @@ export default function Home() {
         <PortfolioSection />
       </AnimatedSection>
 
-      <AnimatedSection>
-        <Modal />
-      </AnimatedSection>
+      {/* <AnimatedSection> */}
+      <Modal />
+      {/* </AnimatedSection> */}
 
       <AnimatedSection>
         <ReviewComponent />
